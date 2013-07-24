d907ee652f249f5b2c8493f52c101b5f08b6bc0c
datsuns <the.skeleton7@gmail.com>
2013-07-25 01:28:32 +0900

    apply received source

---
+++ fizzbuzz.cpp
@@ -0,0 +1,41 @@
+#include "fizzbuzz.h"
+
+using namespace std;
+
+const string FIZZ      = "fizz";
+const string BUZZ      = "buzz";
+const string FIZZ_BUZZ = "fizzbuzz";
+const int STR_LEN_MAX  = 10;
+
+/*
+ * 工夫したことなどなんでもアピール！
+ */
+
+bool isFizz(int n)
+{
+    return (n % 3 == 0);
+}
+
+bool isBuzz(int n)
+{
+    return (n % 5 == 0);
+}
+
+const string& createStr(int n)
+{
+    static string strng;
+    char str[STR_LEN_MAX];
+    snprintf(str,STR_LEN_MAX,"%d",n);
+    strng = str;
+    return strng;
+}
+
+const std::string& fizzbuzz(int n){
+    return isFizz(n) ? isBuzz(n) ? FIZZ_BUZZ
+                                 : FIZZ
+                     : isBuzz(n) ? BUZZ
+                                 : createStr(n);
+}
+
+
+
